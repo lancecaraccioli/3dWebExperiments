16b90313739f14dc1b55cd0db8589e3339b98299
John Terrell <terrellsmooth@gmail.com>
2014-01-29 12:16:59 -0600

    changes to remove large blender files that phpstorm was struggling with indexing causing memory issues in phpstorm

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
  */
 
 
-var camera, scene, renderer;
+var myCamera, scene, renderer;
 var geometry, material, mesh;
 var speed = 10;
 (function($){
@@ -16,12 +16,13 @@ var speed = 10;
         });
 
         function pointerLockChange(event){
+            var renderer = getRenderer();
             if (document.pointerLockElement === renderer.domElement ||
                 document.mozPointerLockElement === renderer.domElement ||
                 document.webkitPointerLockElement === renderer.domElement) {
                 // Pointer was just locked
                 // Enable the mousemove listener
-                document.addEventListener("mousemove", pointerMove, false);
+                //document.addEventListener("mousemove", pointerMove, false);
                 animate();
             } else {
                 // Pointer was just unlocked
@@ -40,37 +41,68 @@ var speed = 10;
             var  deltaX = ((event.webkitMovementX)/ window.innerWidth) * 2*Math.PI;
             var  deltaY = ((event.webkitMovementY)/ window.innerHeight) * 2*Math.PI;
 
+            var camera = getCamera();
             camera.rotateY(-deltaX);
             camera.rotateX(deltaY);
         }
 
+        function updateScene(geometry, material){
+            var myGeometry = new THREE.CubeGeometry( 200, 200, 200 );
+            var myMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+            console.log(geometry);
+            console.log(myGeometry);
+            console.log(material);
+            console.log(myMaterial); 
+
+            mesh = new THREE.Mesh( geometry, material );
+            getScene().add(mesh);
+        }
+
+        function getScene(){
+            if (!scene){
+                scene = new THREE.Scene();
+            }
+            return scene;
+        }
+
+        function getCamera(){
+            if (!myCamera) {
+                myCamera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+                myCamera.position.z = 1000;
+            }
+            return myCamera;
+        }
+
+        function getRenderer(){
+            if (!renderer){
+                renderer = new THREE.CanvasRenderer();
+                renderer.setSize( window.innerWidth, window.innerHeight );
+            }
+            return renderer;
+        }
+
+        function getLoader(){
+            var loader = new THREE.JSONLoader();
+            return loader;
+        }
 
         function initFullScreen() {
 
             //rotating box
-            geometry = new THREE.CubeGeometry( 200, 200, 200 );
-            material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
-            mesh = new THREE.Mesh( geometry, material );
 
-            var walls = new THREE.CubeGeometry(4000, 4000, 4000);
-            var paint = new THREE.MeshFaceMaterial({color:0x00ff00, wireframe:true});
-            var room = new THREE.Mesh(walls, paint);
+            var loader = getLoader();
+            loader.load( "/js/johnbox.json", updateScene );
 
             //renderer
-            renderer = new THREE.CanvasRenderer();
-            renderer.setSize( window.innerWidth, window.innerHeight );
+            renderer = getRenderer();
 
             //camera
-            camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+            myCamera = getCamera();
 
-            camera.position.z = 1000;
+
 
 
             //scene
-            scene = new THREE.Scene();
-            scene.add( mesh );
-            scene.add(room);
-
             document.body.appendChild( renderer.domElement );
 
             document.addEventListener('pointerlockchange', pointerLockChange, false);
@@ -98,13 +130,13 @@ var speed = 10;
             $(document).keydown(function(event){
                 event.preventDefault();
                 if(event.which == $.ui.keyCode.UPPER_W || event.which == $.ui.keyCode.LOWER_W){
-                    camera.translateZ(-speed);
+                    myCamera.translateZ(-speed);
                 } else if(event.which == $.ui.keyCode.UPPER_A || event.which == $.ui.keyCode.LOWER_A){
-                    camera.translateX(-speed);
+                    myCamera.translateX(-speed);
                 } else if(event.which == $.ui.keyCode.UPPER_S || event.which == $.ui.keyCode.LOWER_S){
-                    camera.translateZ(speed);
+                    myCamera.translateZ(speed);
                 } else if(event.which == $.ui.keyCode.UPPER_D || event.which == $.ui.keyCode.LOWER_D){
-                    camera.translateX(speed);
+                    myCamera.translateX(speed);
                 }
             });
 
@@ -120,7 +152,7 @@ var speed = 10;
             //mesh.rotation.x += 0.01;
             //mesh.rotation.y += 0.01;
 
-            renderer.render( scene, camera );
+            renderer.render( scene, myCamera );
 
         }
     });
